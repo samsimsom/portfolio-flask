585d40a028b8203a253595bb3bc58f69feda7514
samsimsom <samsimsom@gmail.com>
2021-04-12 09:02:14 +0300

    update

--- app/static/js/admin/category/app.js
+++ app/static/js/admin/category/app.js
@@ -1,12 +1,74 @@
-console.log("--- Category App ---");
+console.log("--- Category App - 2 ---");
 
-const form = document.getElementById("category-form");
+let editIsActivated
+let addIsActivated
+
+// DOM Elements
+const form = document.getElementById("add-category-form")
 const categoryList = document.getElementById("category-list");
+const popupAlertDiv = document.getElementById("popup-alert");
+const submitBtn = document.getElementById("add-category-btn");
+const submitBtnContainer = document.getElementById("btn-container");
 
-async function formPost() {
+// Event Listeners
+window.addEventListener("load", WindowLoad);
+function WindowLoad(e) {
+  // Fetch all categories from db
+  getCategory()
+    .then((data) => categoriesListUI(data))
+    .catch((err) => console.log(err));
+}
+
+form.addEventListener("submit", addFormSubmit)
+function addFormSubmit(e) {
+  e.preventDefault();
+
+  addNewCategory()
+    .then((data) => {
+      if (data.Err) {
+        return popupAlert(data.Err.name, "danger");
+      }
+      if (data.Success) {
+        clearForm();
+        return getCategory(data.Success);
+      }
+    })
+    .then((data) => {
+      if (data) {
+        addNewCategoryToUI(data);
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+
+categoryList.addEventListener("click", editDeleteEvent);
+function editDeleteEvent(e) {
+  // EDIT
+  if (e.target.classList.contains("edit")) {
+    console.log('EDIT!')
+  }
+
+  // DELETE
+  if (e.target.classList.contains("delete")) {
+    deleteCategory(e.target.id)
+      .then((data) => {
+        if (data.Err) {
+          popupAlert(data.Err, "danger");
+        } else {
+          removeCategoryElement(data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+
+// Backand Communications
+async function addNewCategory() {
   let entry = {
-    name: form.name.value,
-    description: form.description.value,
+    name: form.name.value.trim(),
+    description: form.description.value.trim(),
   };
 
   const url = `${window.origin}/admin/category/add_category`;
@@ -20,20 +82,30 @@ async function formPost() {
   };
 
   const responce = await fetch(url, options);
-  const data = await responce.json();
-
-  // console.log(data);
-
-  return data;
+  if (!responce.ok) {
+    throw new Error("NOT 2XX RESPONSE");
+  } else {
+    const data = await responce.json();
+    return data;
+  }
 }
 
-async function getCategories() {
-  const url = `${window.origin}/admin/category/get_category`;
+async function getCategory(id) {
+  let url = ``;
+  if (id) {
+    url = `${window.origin}/admin/category/get_category/${id}`;
+  } else {
+    url = `${window.origin}/admin/category/get_category`;
+  }
   const options = { method: "GET" };
-  const responce = await fetch(url, options);
-  const data = await responce.json();
 
-  return data;
+  const responce = await fetch(url, options);
+  if (!responce.ok) {
+    throw new Error("NOT 2XX RESPONSE");
+  } else {
+    const data = await responce.json();
+    return data;
+  }
 }
 
 async function deleteCategory(id) {
@@ -47,75 +119,86 @@ async function deleteCategory(id) {
   };
 
   const responce = await fetch(url, options);
-  const data = await responce.json();
-
-  return data;
+  if (!responce.ok) {
+    throw new Error("NOT 2XX RESPONSE");
+  } else {
+    const data = await responce.json();
+    return data;
+  }
 }
 
-function updateCategoryListUI(data) {
-  categoryList.innerHTML = "";
-  const reversed = data.reverse();
-  data.forEach((category, index) => {
-    const categoryHTML = `
-    <tr>
-    <th scope="row">${index}</th>
-    <td>${category._id.$oid.slice(0, 6)}</td>
-    <td>${category.name}</td>
-    <td>${category.description}</td>
-    <td>${category.slug}</td>
-    <td>
-    <div class="btn-group" role="group" aria-label="Basic example">
-      <button type="button" class="btn btn-success edit" id="${
-        category._id.$oid
-      }">Edit</button>
-      <button type="button" class="btn btn-danger delete" id="${
-        category._id.$oid
-      }">Delete</button>
-    </div>
-    </td>
-    </tr>
-  `;
-    categoryList.innerHTML += categoryHTML;
+/*
+async function editCategory(id) {
+  const url = `${window.origin}/admin/category/edit_category/${id}`;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": form.csrf_token.value,
+    },
+  };
+
+  const responce = await fetch(url, options);
+  if (!responce.ok) {
+    throw new Error("NOT 2XX RESPONSE");
+  } else {
+    const data = await responce.json();
+    return data;
+  }
+}
+*/
+
+// UI Generators and Update
+function categoryHtmlGenerator(category) {
+  const html = `
+  <tr id="${category._id.$oid}">
+  <td>${category._id.$oid}</td>
+  <td>${category.name}</td>
+  <td>${category.description}</td>
+  <td>${category.slug}</td>
+  <td>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-success edit" id="${category._id.$oid}">Edit</button>
+    <button type="button" class="btn btn-danger delete" id="${category._id.$oid}">Delete</button>
+  </div>
+  </td>
+  </tr>
+`;
+  return html;
+}
+
+function addNewCategoryToUI(category) {
+  const html = categoryHtmlGenerator(category);
+  categoryList.insertAdjacentHTML("afterbegin", html);
+}
+
+function categoriesListUI(categories) {
+  categories.forEach((category) => {
+    const html = categoryHtmlGenerator(category);
+    categoryList.insertAdjacentHTML("afterbegin", html);
   });
+}
+
+function removeCategoryElement(category) {
+  let removed = document.getElementById(category.id);
+  removed.remove();
+}
+
+function popupAlert(err, style) {
+  popupAlertDiv.innerHTML = "";
+  const html = `
+  <div class="alert alert-${style} alert-dismissible fade show" role="alert">
+  ${err}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `;
+  popupAlertDiv.insertAdjacentHTML("afterbegin", html);
+
+  setTimeout(() => {
+    popupAlertDiv.innerHTML = "";
+  }, 3000);
 }
 
 function clearForm() {
   form.reset();
 }
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  async function glue() {
-    const a = await formPost();
-    const b = await getCategories();
-
-    // console.log(a);
-
-    return { a, b };
-  }
-
-  glue()
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-
-  clearForm();
-});
-
-categoryList.addEventListener("mouseup", (e) => {
-  if (e.target.classList.contains("edit")) {
-    console.log(e.target.id);
-  }
-
-  if (e.target.classList.contains("delete")) {
-    deleteCategory(e.target.id)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  }
-});
-
-window.addEventListener("load", () => {
-  getCategories()
-    .then((data) => updateCategoryListUI(data))
-    .catch((err) => console.log(err));
-});
