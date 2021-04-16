6828b657e33a5cce73da879ce822599a94d374a6
samsimsom <samsimsom@gmail.com>
2021-04-16 16:32:39 +0300

    rich text editor

--- app/static/js/admin/post/post.js
+++ app/static/js/admin/post/post.js
@@ -1,17 +1,13 @@
 console.log('--- POST ---')
-// console.log(current_user)
+console.log(current_user)
 
 // Dropzone Settings
 Dropzone.options.newPostDropzone = {
   paramName: 'file',
-  maxFilesize: 4, // MB
+  maxFilesize: 4,
   addRemoveLinks: true,
-  accept: function (file, done) {
-    if (file.name == 'justinbieber.jpg') {
-      done("Naha, you don't.")
-    } else {
-      done()
-    }
+  accept: (file, done) => {
+    done()
   },
   renameFile: (file) => {
     console.log(file.name)
@@ -23,6 +19,10 @@ Dropzone.options.newPostDropzone = {
   init: function () {
     this.on('addedfile', (file) => {
       console.log('Added in Dropzone! ->', file.name)
+    })
+
+    this.on('success', (file) => {
+      console.log('Success! ->', file.name)
     })
 
     // this.on('complete', (file) => {
